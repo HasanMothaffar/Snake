d8e663f8617e10b8208cd7bbaad849d96999957e
Hasan Mothaffar <hasan.mozafar@gmail.com>
2021-01-03 18:36:15 +0200

    Added documentation for class Canvas

--- js/classes/Canvas.js
+++ js/classes/Canvas.js
@@ -1,13 +1,12 @@
 export default class Canvas {
 
 	/* -- private fields -- */
-	_board;
-	_ctx;
+	_board; //canvas element
+	_ctx; //2d context
 	_boardBackground;
-	_boardBorder;
 	_width;
 	_height;
-	_element;
+	_element; // element to render
 	
 	constructor({
 		id = 'canvas',
@@ -15,32 +14,41 @@ export default class Canvas {
 		width = 300,
 		height = 300,
 		boardBackground = 'white',
-		boardBorder = 'black',
+		boardBorder = 'silver',
 		element = {}
 	}) {
 		this._board = document.getElementById(id);
-		/** @type {CanvasRenderingContext2D} */
 		this._ctx = this._board.getContext('2d');
 		this._element = element;
 
 		this._width = width;
 		this._height = height;
 		this._boardBackground = boardBackground;
-		this._boardBorder = boardBorder;
 	}
 
+	/**
+	 * @returns {number}
+	 */
 	get width() { return this._width; }
 
+	/**
+	 * @returns {number}
+	 */
 	get height() { return this._height; }
 
+	/**
+	 * clears the canvas
+	 * @returns {void}
+	 */
 	clear() {
-		this._ctx.strokestyle = this._boardBorder;
 		this._ctx.fillStyle = this._boardBackground;
-		
 		this._ctx.clearRect(0, 0, this._width, this._height);
-		this._ctx.strokeRect(0, 0, this._width, this._height);
 	}
 
+	/**
+	 * calls the element's rendering function
+	 * @param {any} element - element to render 
+	 */
 	drawElement(element) {
 		element.drawOnCanvas(this._ctx);
 	}
